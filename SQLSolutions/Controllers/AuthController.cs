25e4fcdbde44c4eab6b21047b3de028462e3b574
Andrew Aurzada <aaurzada@gmail.com>
2015-03-05 00:34:00 -0600

    Layouts and views created.

--- SQLSolutions/Controllers/AuthController.cs
+++ SQLSolutions/Controllers/AuthController.cs
@@ -11,7 +11,7 @@ namespace SQLSolutions.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return Content("Login!");
+            return View();
         }
     }
 }
