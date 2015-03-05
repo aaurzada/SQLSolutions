619f0164e3a3751e26e7a7165545eaf4b04e4cb7
Andrew Aurzada <aaurzada@gmail.com>
2015-03-04 23:52:00 -0600

    Created initial routes, controllers, and admin area.

--- SQLSolutions/App_Start/RouteConfig.cs
+++ SQLSolutions/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SQLSolutions.Controllers;
 
 namespace SQLSolutions
 {
@@ -11,13 +12,11 @@ namespace SQLSolutions
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //var namespaces = new[] { typeof(HomeController).Namespace};
+            
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new {controller = "Auth", action = "Login"});
         }
     }
 }
